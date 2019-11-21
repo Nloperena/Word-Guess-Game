777b4d4bf0da792e1ceabc9822b2e67933d8662c
Nicholas Loperena <nicholasloperena@gmail.com>
2019-11-20 21:03:26 -0500

    added more logic to my app.js, takes in user input and randomizes the pool of words available

--- app.js
+++ app.js
@@ -1,17 +1,57 @@
-window.onload = function () {
 
+// VARIABLES
+//=====================
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
           'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
           't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
           
-          
-          var chosenCategory;     // Selected catagory
-          var getHint ;          // Word getHint
-          var word ;              // Selected word
-          var guess ;             // Geuss
-          var geusses = [ ];      // Stored geusses
-          var lives ;             // Lives
-          var counter ;           // Count correct geusses
-          var space;              // Number of spaces in a word
+
+
+    var wordBank = ['red', 'blue', 'yellow', 'orange', 'black', 'grey', 'pink', 'cheese', 'milk', 'oyster', 'banana', 'apple' ];
+    var wins = 0;
+    var loss = 0;
+    var wrongLetter = [];
+    var guessesLeft = 9;
+    var underScores = [];
+    var userGuesses = [];
+    var randomWord;
+
+// FUNCTION
+//=====================
+function startGame() {
+    randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log(randomWord);
+
+    for(var i = 0; i < randomWord.length; i++)
+    {
+        underScores.push('__');
+    }
+    document.getElementById('underscores').textContent = underScores.join(' ');
+
+    //GAME RESET
+    //==========
+    wrongLetter = []
+    guessesLeft = 9;
+
+    //HTML
+    //==========
+    document.getElementById('guesses-left').textContent = guessesLeft;
+}
+    //Registering input on keyboard
+    //==========
+document.onkeyup = function(event) 
+{
+    userGuesses = event.key;
+
+    if(randomWord.indexOf(userGuesses) > -1)
+    {
+        console.log(randomWord.indexOf(userGuesses));
+    }
+
+}
+
+//MAIN
+//=====================
+startGame()
